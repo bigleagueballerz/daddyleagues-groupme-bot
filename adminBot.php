4a17fa86ab714af5009fdfab6cf119eef826b06b
bigleagueballerz <bigleagueballerz@gmail.com>
2017-06-01 09:30:23 -0500

    Update adminBot.php

--- adminBot.php
+++ adminBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "admin bot token";
+$bot_token = "de82c43fb54d74f47a40700d88";
 
 // adds admin commands 
 $isAdmin = TRUE;
